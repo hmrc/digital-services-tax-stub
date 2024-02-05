477ae052a5180dd56ec681702e6d61c0c15190d5
Tapiwa Tiyemba <142234222+tapiwa-tiyemba@users.noreply.github.com>
2024-02-05 14:53:56 +0000

    small fix

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,7 +19,8 @@ object AppDependencies {
     "uk.gov.hmrc"         %% s"bootstrap-test-play-$play" % hmrcBootstrapVersion % Test,
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.6"             % Test,
     "org.pegdown"          % "pegdown"                % "1.6.0"              % Test,
-    "org.scalatestplus"   %% "mockito-3-12"           % "3.2.10.0"           % Test
+    "org.scalatestplus"   %% "mockito-3-12"           % "3.2.10.0"           % Test,
+    "uk.gov.hmrc" %% "stub-data-generator"       % "1.1.0" % Test
   )
 
 }
