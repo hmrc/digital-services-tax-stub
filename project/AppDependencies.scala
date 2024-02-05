694e1491f6cc129d1806ab39a0fd13b5b7bb938b
Tapiwa Tiyemba <142234222+tapiwa-tiyemba@users.noreply.github.com>
2024-02-05 15:03:14 +0000

    cleanup

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,3 @@
-import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
@@ -14,13 +11,11 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "stub-data-generator"    % "1.1.0"              % Test,
     "org.scalatest"          %% "scalatest"                   % "3.2.17" % Test,
     "uk.gov.hmrc"         %% s"bootstrap-test-play-$play" % hmrcBootstrapVersion % Test,
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.6"             % Test,
     "org.pegdown"          % "pegdown"                % "1.6.0"              % Test,
     "org.scalatestplus"   %% "mockito-3-12"           % "3.2.10.0"           % Test,
-    "uk.gov.hmrc" %% "stub-data-generator"       % "1.1.0" % Test
   )
 
 }
